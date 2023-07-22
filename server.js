a3388a19c1e3965327204ea6e5c371ecac5ec35b
Adam Harvey <heyitsmeharv@gmail.com>
2023-07-22 11:26:26 +0200

    Connect to the database before listening

--- server.js
+++ server.js
@@ -11,15 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+    console.log(`MongoDB Connected: ${mongoose}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
+});
+
+connectDB().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-  });
-});
+  })
+})
 
 const commentsRouter = require('./routes/comments');
 const emailRouter = require('./routes/email');
